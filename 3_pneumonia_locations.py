71905fb0cb960d69de5274866d7bbceefd5b0f66
chonorat <47229595+chonorat@users.noreply.github.com>
2019-05-05 12:47:20 -0400

    Update 3_pneumonia_locations.py

--- 3_pneumonia_locations.py
+++ 3_pneumonia_locations.py
@@ -4,14 +4,15 @@
 # In[ ]:
 
 
-folder="/content/train_images/"
-pneum_locs=[]
-filenames=[]
-for filename in glob.glob("/content/train_images/*.dcm"):
-  ds=pydicom.read_file(filename)
-  boxes = info1[(info1['patientId'] == str(ds[0x010, 0x010].value)) & (info1['class'] == 'Lung Opacity')]
-  pneumonia_loc = boxes.iloc[:,2:6].values
-  filenames.append(str(ds[0x010, 0x010].value))
-  if len(pneumonia_loc)>0:
-    pneum_locs.append([str(ds[0x010, 0x010].value),pneumonia_loc])
+
+pneum_locs={}
+for i in range(1,len(labels)):
+  target=labels.Target[i]
+  if target==1:
+    name=labels.patientId[i]
+    location=labels.x[i],labels.y[i],labels.width[i],labels.height[i]
+    if name in pneum_locs:
+      pneum_locs[name].append(location)
+    else:
+      pneum_locs[name] = [location]
 
